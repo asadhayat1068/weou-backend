b6899995e6894c74378c700926e16e43488dcf67
Asad Hayat <asadhayat.1068@gmail.com>
2024-05-27 04:13:24 +0500

    Added route for nonce and message verification

--- src/controllers/siwe/siwe.controller.ts
+++ src/controllers/siwe/siwe.controller.ts
@@ -1,13 +1,36 @@
 import { Request, Response } from "express";
 import { SuccessResponse } from "../../util";
-import { generateNonce } from "siwe";
+import { SiweMessage, generateNonce } from "siwe";
 
 export class SIWEController {
   static getNonce(req: Request, res: Response) {
-    const nonce = generateNonce();
+    let nonce;
+    const visited = req.session?.visited ?? false;
+    if (!visited) {
+      console.log("Not visited");
+      req.session.visited = true;
+      nonce = generateNonce();
+      req.session.nonce = nonce;
+    } else {
+      console.log("Visited");
+      nonce = req.session.nonce;
+    }
+
     const response = new SuccessResponse(res, "Nonce.", {
       nonce,
     });
     return response.send();
+  }
+
+  static async verifyMessage(req: Request, res: Response) {
+    const { message, signature } = req.body;
+    const siweMessage = new SiweMessage(message);
+    try {
+      const v = await siweMessage.verify({ signature });
+      console.log("Verify Result:", v);
+      res.send(true);
+    } catch {
+      res.send(false);
+    }
   }
 }
