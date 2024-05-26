dfde94ff0d611e1b0398ff53771dd38962035970
Asad Hayat <asadhayat.1068@gmail.com>
2024-05-27 04:13:06 +0500

    Setup express-session

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,25 @@ import { AppRouter } from "./routes";
 import { LoggingStream } from "./util/logger";
 import { NotFoundError } from "./util";
 import { errorHandler } from "./middleware";
+import session from "express-session";
+declare module "express-session" {
+  interface SessionData {
+    visited: boolean;
+    nonce: string;
+  }
+}
 
 export const app: express.Application = express();
+app.use(
+  session({
+    secret: "ABCD",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1 * 60 * 60 * 1000,
+    },
+  })
+);
 app.use(ruid());
 app.use(express.json());
 morganBody(app, {
