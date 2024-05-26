a63a77216279609816dcbeca9fe31a17e015fc76
Asad Hayat <asadhayat.1068@gmail.com>
2024-05-27 02:50:59 +0500

    SIWE: Added siwe package, updated ethers and fixed bugs as per ethersjs updated version

--- src/config/network.ts
+++ src/config/network.ts
@@ -1,13 +1,11 @@
-import { ethers } from "ethers";
+import { WebSocketProvider, ethers } from "ethers";
 import dotenv from "dotenv";
 dotenv.config();
 
 const providerURL_WSS = process.env.WSS_URL || "";
 
 export const networkConfig = {};
-export const wss_provider = new ethers.providers.WebSocketProvider(
-  providerURL_WSS
-);
+export const wss_provider = new WebSocketProvider(providerURL_WSS);
 
 export const COLLECTION_ADDRESS = process.env.COLLECTION_ADDRESS || "";
 export const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY || "";
